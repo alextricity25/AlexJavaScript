3b4be3a9a6eb743ea910847272326d2ef9a820f5
Miguel Alex Cantu <miguel.can2@gmail.com>
2020-06-13 19:16:44 -0500

    Learning Concurrency in JS!

---
+++ Promises/promise_all.js
@@ -0,0 +1,12 @@
+let myPromises =
+	Promise.all([returnsPromOne(),
+		returnsPromTwo(),
+		returnsPromThree()]);
+
+myPromises
+	.then((arrayOfValues) => {
+		console.log(arrayOfValues);
+	})
+	.catch((rejectionReason) => {
+		console.log(rejectionReason);
+	});
